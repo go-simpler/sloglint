ba82b19e39fcc6c445911eff21a37ced4acc6e91
Tom <73077675+tmzane@users.noreply.github.com>
2023-09-15 00:15:38 +0300

    test: add custom `slog.Attr` constructor case

--- testdata/src/no_raw_keys/no_raw_keys.go
+++ testdata/src/no_raw_keys/no_raw_keys.go
@@ -4,17 +4,17 @@ import (
 	"log/slog"
 )
 
-const (
-	foo = "foo"
-	bar = "bar"
-)
+const foo = "foo"
+
+func Foo(value int) slog.Attr {
+	return slog.Int("foo", value)
+}
 
 func tests() {
 	slog.Info("msg", foo, 1)
+	slog.Info("msg", Foo(1))
 	slog.Info("msg", slog.Int(foo, 1))
-	slog.Info("msg", foo, 1, slog.Int(bar, 2)) // want `key-value pairs and attributes should not be mixed`
 
-	slog.Info("msg", "foo", 1)                     // want `raw keys should not be used`
-	slog.Info("msg", slog.Int("foo", 1))           // want `raw keys should not be used`
-	slog.Info("msg", "foo", 1, slog.Int("bar", 2)) // want `raw keys should not be used` `key-value pairs and attributes should not be mixed`
+	slog.Info("msg", "foo", 1)           // want `raw keys should not be used`
+	slog.Info("msg", slog.Int("foo", 1)) // want `raw keys should not be used`
 }
