3c27de040cf1467c5216102606232e5adb73f1e5
Tom <73077675+tmzane@users.noreply.github.com>
2023-10-15 01:34:32 +0300

    feat: implement `-args-on-sep-lines`

---
+++ testdata/src/args_on_sep_lines/args_on_sep_lines.go
@@ -0,0 +1,19 @@
+package args_on_sep_lines
+
+import "log/slog"
+
+func tests() {
+	slog.Info("msg", "foo", 1)
+	slog.Info("msg",
+		"foo", 1,
+		"bar", 2,
+	)
+
+	slog.Info("msg", "foo", 1, "bar", 2) // want `arguments should be put on separate lines`
+	slog.Info("msg", "foo", 1,           // want `arguments should be put on separate lines`
+		"bar", 2,
+	)
+	slog.Info("msg", // want `arguments should be put on separate lines`
+		"foo", 1, "bar", 2,
+	)
+}
