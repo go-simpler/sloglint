f4014ddd282000808023514d58a198e040878228
Tom <73077675+tmzane@users.noreply.github.com>
2024-04-20 21:41:04 +0500

    feat: implement `-context-only=scope` (#33)

---
+++ testdata/src/context_only_all/context_only_all.go
@@ -0,0 +1,31 @@
+package context_only_all
+
+import (
+	"context"
+	"log/slog"
+)
+
+func tests(ctx context.Context) {
+	slog.Log(ctx, slog.LevelInfo, "msg")
+	slog.DebugContext(ctx, "msg")
+	slog.InfoContext(ctx, "msg")
+	slog.WarnContext(ctx, "msg")
+	slog.ErrorContext(ctx, "msg")
+
+	slog.Debug("msg") // want `DebugContext should be used instead`
+	slog.Info("msg")  // want `InfoContext should be used instead`
+	slog.Warn("msg")  // want `WarnContext should be used instead`
+	slog.Error("msg") // want `ErrorContext should be used instead`
+
+	logger := slog.New(nil)
+	logger.Log(ctx, slog.LevelInfo, "msg")
+	logger.DebugContext(ctx, "msg")
+	logger.InfoContext(ctx, "msg")
+	logger.WarnContext(ctx, "msg")
+	logger.ErrorContext(ctx, "msg")
+
+	logger.Debug("msg") // want `DebugContext should be used instead`
+	logger.Info("msg")  // want `InfoContext should be used instead`
+	logger.Warn("msg")  // want `WarnContext should be used instead`
+	logger.Error("msg") // want `ErrorContext should be used instead`
+}
---
+++ testdata/src/context_only_scope/context_only_scope.go
@@ -0,0 +1,25 @@
+package context_only_scope
+
+import (
+	"context"
+	"log/slog"
+)
+
+func tests(ctx context.Context) {
+	slog.Info("msg") // want `InfoContext should be used instead`
+	slog.InfoContext(ctx, "msg")
+
+	if true {
+		slog.Info("msg") // want `InfoContext should be used instead`
+		slog.InfoContext(ctx, "msg")
+	}
+
+	_ = func() {
+		slog.Info("msg") // want `InfoContext should be used instead`
+		slog.InfoContext(ctx, "msg")
+	}
+}
+
+func noctx() {
+	slog.Info("msg")
+}
