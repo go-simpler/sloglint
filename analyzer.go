e8d4a13b6d99c52bd90f9fe08f7ba6ac9d31389f
Nick <73077675+tmzane@users.noreply.github.com>
2026-02-11 00:33:51 +0200

    dev(all): split parser and checks

--- analyzer.go
+++ analyzer.go
@@ -2,18 +2,12 @@
 package sloglint
 
 import (
-	"fmt"
 	"go/ast"
 	"go/token"
 	"go/types"
-	"go/version"
 	"iter"
-	"slices"
 	"strconv"
-	"strings"
-	"unicode"
 
-	"github.com/ettle/strcase"
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/passes/inspect"
 	"golang.org/x/tools/go/ast/inspector"
@@ -47,33 +41,36 @@ func New(opts *Options) *analysis.Analyzer {
 	}
 }
 
-var slogFuncs = map[string]struct {
-	argsPos   int  // The position of key-value arguments in the function signature, starting from 0.
-	hasMsgArg bool // Whether the function has the "msg" argument that should be analyzed.
-	hasCtxAlt bool // Whether an alternative function that accepts a context as the first argument exists.
-}{
-	"log/slog.With":                   {argsPos: 0},
-	"log/slog.Log":                    {argsPos: 3, hasMsgArg: true},
-	"log/slog.LogAttrs":               {argsPos: 3, hasMsgArg: true},
-	"log/slog.Debug":                  {argsPos: 1, hasMsgArg: true, hasCtxAlt: true},
-	"log/slog.Info":                   {argsPos: 1, hasMsgArg: true, hasCtxAlt: true},
-	"log/slog.Warn":                   {argsPos: 1, hasMsgArg: true, hasCtxAlt: true},
-	"log/slog.Error":                  {argsPos: 1, hasMsgArg: true, hasCtxAlt: true},
-	"log/slog.DebugContext":           {argsPos: 2, hasMsgArg: true},
-	"log/slog.InfoContext":            {argsPos: 2, hasMsgArg: true},
-	"log/slog.WarnContext":            {argsPos: 2, hasMsgArg: true},
-	"log/slog.ErrorContext":           {argsPos: 2, hasMsgArg: true},
-	"(*log/slog.Logger).With":         {argsPos: 0},
-	"(*log/slog.Logger).Log":          {argsPos: 3, hasMsgArg: true},
-	"(*log/slog.Logger).LogAttrs":     {argsPos: 3, hasMsgArg: true},
-	"(*log/slog.Logger).Debug":        {argsPos: 1, hasMsgArg: true, hasCtxAlt: true},
-	"(*log/slog.Logger).Info":         {argsPos: 1, hasMsgArg: true, hasCtxAlt: true},
-	"(*log/slog.Logger).Warn":         {argsPos: 1, hasMsgArg: true, hasCtxAlt: true},
-	"(*log/slog.Logger).Error":        {argsPos: 1, hasMsgArg: true, hasCtxAlt: true},
-	"(*log/slog.Logger).DebugContext": {argsPos: 2, hasMsgArg: true},
-	"(*log/slog.Logger).InfoContext":  {argsPos: 2, hasMsgArg: true},
-	"(*log/slog.Logger).WarnContext":  {argsPos: 2, hasMsgArg: true},
-	"(*log/slog.Logger).ErrorContext": {argsPos: 2, hasMsgArg: true},
+type slogFuncInfo struct {
+	msgPos  int // The position of the "msg string" argument in the function signature, starting from 0.
+	argsPos int // The position of the "args ...any" argument in the function signature, starting from 0.
+}
+
+var slogFuncs = map[string]slogFuncInfo{
+	"log/slog.Log":                    {msgPos: 2, argsPos: 3},
+	"log/slog.LogAttrs":               {msgPos: 2, argsPos: 3},
+	"log/slog.Debug":                  {msgPos: 0, argsPos: 1},
+	"log/slog.Info":                   {msgPos: 0, argsPos: 1},
+	"log/slog.Warn":                   {msgPos: 0, argsPos: 1},
+	"log/slog.Error":                  {msgPos: 0, argsPos: 1},
+	"log/slog.DebugContext":           {msgPos: 1, argsPos: 2},
+	"log/slog.InfoContext":            {msgPos: 1, argsPos: 2},
+	"log/slog.WarnContext":            {msgPos: 1, argsPos: 2},
+	"log/slog.ErrorContext":           {msgPos: 1, argsPos: 2},
+	"log/slog.With":                   {msgPos: -1, argsPos: 0},
+	"log/slog.NewTextHandler":         {msgPos: -1, argsPos: -1},
+	"log/slog.NewJSONHandler":         {msgPos: -1, argsPos: -1},
+	"(*log/slog.Logger).Log":          {msgPos: 2, argsPos: 3},
+	"(*log/slog.Logger).LogAttrs":     {msgPos: 2, argsPos: 3},
+	"(*log/slog.Logger).Debug":        {msgPos: 0, argsPos: 1},
+	"(*log/slog.Logger).Info":         {msgPos: 0, argsPos: 1},
+	"(*log/slog.Logger).Warn":         {msgPos: 0, argsPos: 1},
+	"(*log/slog.Logger).Error":        {msgPos: 0, argsPos: 1},
+	"(*log/slog.Logger).DebugContext": {msgPos: 1, argsPos: 2},
+	"(*log/slog.Logger).InfoContext":  {msgPos: 1, argsPos: 2},
+	"(*log/slog.Logger).WarnContext":  {msgPos: 1, argsPos: 2},
+	"(*log/slog.Logger).ErrorContext": {msgPos: 1, argsPos: 2},
+	"(*log/slog.Logger).With":         {msgPos: -1, argsPos: 0},
 }
 
 var attrFuncs = map[string]struct{}{
@@ -97,73 +94,19 @@ func analyze(pass *analysis.Pass, opts *Options, cursor inspector.Cursor) {
 		return
 	}
 
-	name := fn.FullName()
-
-	if name == "log/slog.NewTextHandler" || name == "log/slog.NewJSONHandler" {
-		checkDiscardHandler(pass, call)
-	}
-
-	info, ok := slogFuncs[name]
+	info, ok := slogFuncs[fn.FullName()]
 	if !ok {
 		return
 	}
 
-	switch opts.NoGlobal {
-	case noGlobalAll:
-		if strings.HasPrefix(name, "log/slog.") || isGlobalLoggerUsed(pass.TypesInfo, call.Fun) {
-			pass.Reportf(call.Pos(), "global logger should not be used")
-		}
-	case noGlobalDefault:
-		if strings.HasPrefix(name, "log/slog.") {
-			pass.Reportf(call.Pos(), "default logger should not be used")
-		}
+	var msg ast.Expr
+	if info.msgPos >= 0 && len(call.Args) > info.msgPos {
+		msg = call.Args[info.msgPos]
 	}
 
-	if info.hasCtxAlt {
-		switch opts.ContextOnly {
-		case contextOnlyAll:
-			typ := pass.TypesInfo.TypeOf(call.Args[0])
-			if typ != nil && typ.String() != "context.Context" {
-				pass.Reportf(call.Pos(), "%sContext should be used instead", fn.Name())
-			}
-		case contextOnlyScope:
-			if isContextInScope(pass.TypesInfo, cursor) {
-				typ := pass.TypesInfo.TypeOf(call.Args[0])
-				if typ != nil && typ.String() != "context.Context" {
-					pass.Reportf(call.Pos(), "%sContext should be used instead", fn.Name())
-				}
-			}
-		}
-	}
-
-	if info.hasMsgArg {
-		msgPos := info.argsPos - 1
-
-		if opts.StaticMsg && msgPos >= 0 && !isStaticMsg(pass.TypesInfo, call.Args[msgPos]) {
-			pass.Reportf(call.Args[msgPos].Pos(), "message should be a string literal or a constant")
-		}
-
-		if opts.MsgStyle != "" && msgPos >= 0 {
-			if lit, ok := call.Args[msgPos].(*ast.BasicLit); ok && lit.Kind == token.STRING {
-				value, err := strconv.Unquote(lit.Value)
-				if err != nil {
-					panic("unreachable") // String literals are always quoted.
-				}
-				if ok := isValidMsgStyle(value, opts.MsgStyle); !ok {
-					pass.Reportf(call.Args[msgPos].Pos(), "message should be %s", opts.MsgStyle)
-				}
-			}
-		}
-
-	}
-
-	if len(call.Args) < info.argsPos {
-		return
-	}
-
-	args := call.Args[info.argsPos:]
-	if len(args) == 0 {
-		return
+	var args []ast.Expr
+	if info.argsPos >= 0 && len(call.Args) > info.argsPos {
+		args = call.Args[info.argsPos:]
 	}
 
 	var keys []ast.Expr
@@ -191,209 +134,15 @@ func analyze(pass *analysis.Pass, opts *Options, cursor inspector.Cursor) {
 		}
 	}
 
-	switch {
-	case opts.KVOnly && len(attrs) > 0:
-		pass.Reportf(call.Pos(), "attributes should not be used")
-	case opts.AttrOnly && len(keys) > 0:
-		pass.Reportf(call.Pos(), "key-value pairs should not be used")
-	case opts.NoMixedArgs && len(attrs) > 0 && len(keys) > 0:
-		pass.Reportf(call.Pos(), "key-value pairs and attributes should not be mixed")
-	}
-
-	if opts.NoRawKeys {
-		for key := range allKeys(pass.TypesInfo, keys, attrs) {
-			if sel, ok := key.(*ast.SelectorExpr); ok {
-				key = sel.Sel // The key is defined in another package, e.g. pkg.ConstKey.
-			}
-
-			isConst := false
-
-			if ident, ok := key.(*ast.Ident); ok {
-				if obj := pass.TypesInfo.ObjectOf(ident); obj != nil {
-					if _, ok := obj.(*types.Const); ok {
-						isConst = true
-					}
-				}
-			}
-
-			if !isConst {
-				pass.Reportf(key.Pos(), "raw keys should not be used")
-			}
-		}
-	}
-
-	checkKeysNaming(opts, pass, keys, attrs)
-
-	if len(opts.AllowedKeys) > 0 {
-		for key := range allKeys(pass.TypesInfo, keys, attrs) {
-			if name, ok := getKeyName(key); ok && !slices.Contains(opts.AllowedKeys, name) {
-				pass.Reportf(key.Pos(), "%q key is not in the allowed keys list and should not be used", name)
-			}
-		}
-	}
-
-	if len(opts.ForbiddenKeys) > 0 {
-		for key := range allKeys(pass.TypesInfo, keys, attrs) {
-			if name, ok := getKeyName(key); ok && slices.Contains(opts.ForbiddenKeys, name) {
-				pass.Reportf(key.Pos(), "%q key is forbidden and should not be used", name)
-			}
-		}
-	}
-
-	if opts.ArgsOnSepLines && areArgsOnSameLine(pass.Fset, call, keys, attrs) {
-		pass.Reportf(call.Pos(), "arguments should be put on separate lines")
-	}
-}
-
-func checkKeysNaming(opts *Options, pass *analysis.Pass, keys, attrs []ast.Expr) {
-	checkKeyNamingCase := func(caseFn func(string) string, caseName string) {
-		for key := range allKeys(pass.TypesInfo, keys, attrs) {
-			name, ok := getKeyName(key)
-			if !ok || name == caseFn(name) {
-				return
-			}
-
-			pass.Report(analysis.Diagnostic{
-				Pos:     key.Pos(),
-				Message: fmt.Sprintf("keys should be written in %s", caseName),
-				SuggestedFixes: []analysis.SuggestedFix{{
-					TextEdits: []analysis.TextEdit{{
-						Pos:     key.Pos(),
-						End:     key.End(),
-						NewText: []byte(strconv.Quote(caseFn(name))),
-					}},
-				}},
-			})
-		}
-	}
-
-	switch opts.KeyNamingCase {
-	case keyNamingCaseSnake:
-		checkKeyNamingCase(strcase.ToSnake, "snake_case")
-	case keyNamingCaseKebab:
-		checkKeyNamingCase(strcase.ToKebab, "kebab-case")
-	case keyNamingCaseCamel:
-		checkKeyNamingCase(strcase.ToCamel, "camelCase")
-	case keyNamingCasePascal:
-		checkKeyNamingCase(strcase.ToPascal, "PascalCase")
-	}
-}
-
-func checkDiscardHandler(pass *analysis.Pass, call *ast.CallExpr) {
-	switch v := pass.Module.GoVersion; {
-	case v == "": // Empty in test runs.
-	case version.Compare("go"+v, "go1.24") >= 0:
-	default:
-		return
-	}
-
-	sel, ok := call.Args[0].(*ast.SelectorExpr)
-	if !ok {
-		return
-	}
-
-	obj := pass.TypesInfo.ObjectOf(sel.Sel)
-	if obj == nil {
-		return
-	}
-
-	if obj.Pkg().Name() != "io" || obj.Name() != "Discard" {
-		return
-	}
-
-	pass.Report(analysis.Diagnostic{
-		Pos:     call.Pos(),
-		Message: "use slog.DiscardHandler instead",
-		SuggestedFixes: []analysis.SuggestedFix{{
-			TextEdits: []analysis.TextEdit{{
-				Pos:     call.Pos(),
-				End:     call.End(),
-				NewText: []byte("slog.DiscardHandler"),
-			}},
-		}},
-	})
-}
-
-func isGlobalLoggerUsed(info *types.Info, call ast.Expr) bool {
-	sel, ok := call.(*ast.SelectorExpr)
-	if !ok {
-		return false
-	}
-
-	ident, ok := sel.X.(*ast.Ident)
-	if !ok {
-		return false
-	}
-
-	obj := info.ObjectOf(ident)
-	return obj.Parent() == obj.Pkg().Scope()
-}
-
-func isContextInScope(info *types.Info, cursor inspector.Cursor) bool {
-	for c := range cursor.Enclosing(new(ast.FuncDecl), new(ast.FuncLit)) {
-		var params []*ast.Field
-
-		switch fn := c.Node().(type) {
-		case *ast.FuncDecl:
-			params = fn.Type.Params.List
-		case *ast.FuncLit:
-			params = fn.Type.Params.List
-		}
-
-		if len(params) == 0 || len(params[0].Names) == 0 {
-			continue
-		}
-
-		typ := info.TypeOf(params[0].Names[0])
-		if typ != nil && (typ.String() == "context.Context" || typ.String() == "*net/http.Request") {
-			return true
-		}
-	}
-
-	return false
-}
-
-func isStaticMsg(info *types.Info, msg ast.Expr) bool {
-	switch msg := msg.(type) {
-	case *ast.BasicLit: // e.g. slog.Info("msg")
-		return msg.Kind == token.STRING
-	case *ast.Ident: // e.g. slog.Info(constMsg)
-		_, isConst := info.ObjectOf(msg).(*types.Const)
-		return isConst
-	case *ast.BinaryExpr: // e.g. slog.Info("x" + "y")
-		if msg.Op != token.ADD {
-			panic("unreachable") // Only "+" can be applied to strings.
-		}
-		return isStaticMsg(info, msg.X) && isStaticMsg(info, msg.Y)
-	default:
-		return false
-	}
-}
-
-func isValidMsgStyle(msg, style string) bool {
-	runes := []rune(strings.TrimSpace(msg))
-	if len(runes) < 2 {
-		return true
-	}
-
-	first, second := runes[0], runes[1]
-
-	switch style {
-	case msgStyleLowercased:
-		if unicode.IsLower(first) {
-			return true
-		}
-		if unicode.IsPunct(second) {
-			return true // e.g. "U.S.A."
-		}
-		return unicode.IsUpper(second) // e.g. "HTTP"
-	case msgStyleCapitalized:
-		if unicode.IsUpper(first) {
-			return true
-		}
-		return unicode.IsUpper(second) // e.g. "iPhone"
-	default:
-		panic("unreachable")
+	for _, check := range checks {
+		check(pass, opts, slogFuncCall{
+			fn:     fn,
+			expr:   call,
+			cursor: cursor,
+			msg:    msg,
+			keys:   keys,
+			attrs:  attrs,
+		})
 	}
 }
 
@@ -454,35 +203,19 @@ func getKeyName(key ast.Expr) (string, bool) {
 			return "", false
 		}
 		if spec, ok := ident.Obj.Decl.(*ast.ValueSpec); ok && len(spec.Values) > 0 {
-			// TODO: Support len(spec.Values) > 1; e.g. const foo, bar = 1, 2.
-			key = spec.Values[0]
+			key = spec.Values[0] // TODO: Support len(spec.Values) > 1; e.g. const foo, bar = 1, 2.
 		}
 	}
-	if lit, ok := key.(*ast.BasicLit); ok && lit.Kind == token.STRING {
-		value, err := strconv.Unquote(lit.Value)
-		if err != nil {
-			panic("unreachable") // String literals are always quoted.
-		}
-		return value, true
-	}
-	return "", false
-}
 
-func areArgsOnSameLine(fset *token.FileSet, call ast.Expr, keys, attrs []ast.Expr) bool {
-	if len(keys)+len(attrs) <= 1 {
-		return false // Special case: slog.Info("msg", "key", "value") is fine.
+	lit, ok := key.(*ast.BasicLit)
+	if !ok || lit.Kind != token.STRING {
+		return "", false
 	}
 
-	args := slices.Concat([]ast.Expr{call}, keys, attrs)
-
-	lines := make(map[int]struct{}, len(args))
-	for _, arg := range args {
-		line := fset.Position(arg.Pos()).Line
-		if _, ok := lines[line]; ok {
-			return true
-		}
-		lines[line] = struct{}{}
+	name, err := strconv.Unquote(lit.Value)
+	if err != nil {
+		panic("unreachable") // String literals are always quoted.
 	}
 
-	return false
+	return name, true
 }
